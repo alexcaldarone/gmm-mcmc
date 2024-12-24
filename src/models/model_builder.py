1a668d13b95c7349e4044122aff3af13f1b78e04
Alex Caldarone <cldrna_coding.is235@aleeas.com>
2024-12-24 15:38:06 +0100

    update model builder

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -1,6 +1,7 @@
 from typing import Union
 import numpy as np
 import pymc as pm
+import pytensor.tensor as pt
 
 from src.utils.gmm_parameters import (
     UnivariateGMMParameters,
@@ -21,8 +22,10 @@ def build_model(model_type: str,
             # Hyper-priors
             mu0 = pm.Normal("mu0", mu=prior_params.mu0_mean, sigma=prior_params.mu0_std)
             
-            # Priors for component means and variances
-            mu_k = pm.Normal("mu_k", mu=mu0, sigma=prior_params.muk_variance, shape=gmm_params.n_components)
+            # Priors for component means and variance
+            mu_k_raw = pm.Normal("mu_k_raw", mu=mu0, sigma=prior_params.muk_variance, shape=gmm_params.n_components)
+            mu_k = pm.Deterministic("mu_k", pt.sort(mu_k_raw))
+
             sigma_k = pm.InverseGamma("sigma_k", alpha=prior_params.sigma0_alpha, beta=prior_params.sigma0_beta, shape=gmm_params.n_components, transform=None)
             
             # Define component distributions
@@ -39,7 +42,9 @@ def build_model(model_type: str,
             mu0 = pm.MvNormal("mu0", mu=prior_params.mu0_mean, cov=prior_params.mu0_std, shape=(prior_params.dimension,)) # have to add shape?
 
             # Priors for component means and variances
-            mu_k = pm.MvNormal("mu_k", mu = mu0, cov = prior_params.muk_variance, shape = (gmm_params.n_components, gmm_params.dimension))
+            mu_k_raw = pm.MvNormal("mu_k", mu = mu0, cov = prior_params.muk_variance, shape = (gmm_params.n_components, gmm_params.dimension))
+            mu_k = pm.Deterministic("mu_k", mu_k_raw)
+                        
             packed_chol = pm.LKJCholeskyCov("packed_chol", n=prior_params.dimension, eta=1, sd_dist=pm.HalfNormal.dist(2), 
                                             compute_corr=False)
             sigma_k = pm.expand_packed_triangular(prior_params.dimension, packed_chol, lower = False)
